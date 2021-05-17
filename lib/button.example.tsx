a732c9a9c2249e9fb9f63c56e8b88af4de0d5f30
angus <tsang1205@qq.com>
2021-05-17 21:22:56 +0800

    feat: add example page

---
+++ lib/button.example.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ButtonExample: React.FC = () => {
+  return <div>button example</div>;
+};
+
+export default ButtonExample;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,10 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = Object.assign({}, bathConfig, {
   mode: 'development',
+  entry: {
+    example: './example.tsx',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'AUI',
-      template: 'index.html',
+      template: 'example.html',
     }),
   ],
 });
